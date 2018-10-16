dd91de4e5285080cb543641fb969eadf0c94b7cd
Tristan Partin <tristan.partin@gmail.com>
2018-10-15 22:44:47 -0500

    Further development on resolvers
    Greg and I pushed forward on further developing resolvers. We added
    a createTopic, and updateUser resolver.

--- bb-server/src/schema/Topic.ts
+++ bb-server/src/schema/Topic.ts
@@ -1,11 +1,15 @@
 import { gql, IResolvers } from "apollo-server-express";
-import { DocumentNode } from "graphql";
-import { getConnection } from "typeorm";
+import { DocumentNode, GraphQLResolveInfo } from "graphql";
+import { DeepPartial, getConnection, Repository } from "typeorm";
 import Topic from "../entities/Topic";
 
 export const typeDef: DocumentNode = gql`
+    extend type Mutation {
+        createTopic(name: TopicInput!): [Topic]
+    }
+
     extend type Query {
-        topic(id: Int!): Topic
+        getTopic(id: Int!): Topic
     }
 
     type Topic {
@@ -18,23 +22,58 @@ export const typeDef: DocumentNode = gql`
     }
 `;
 
-interface ITopicQuery {
-    id: number;
+interface ICreateTopicsInput {
+    topic: DeepPartial<Topic>;
+}
+
+// tslint:disable-next-line: no-any
+function createTopic(parent: any, args: ICreateTopicsInput, ctx: any, info: GraphQLResolveInfo): Promise<DeepPartial<Topic>> {
+    return _createTopic(args.topic);
+}
+
+export async function _createTopic(topic: DeepPartial<Topic>): Promise<DeepPartial<Topic>> {
+    const topicsRepo: Repository<Topic> = getConnection().getRepository(Topic);
+
+    const foundTopic: Topic | undefined = await topicsRepo.createQueryBuilder()
+        .where("topic.name = :name", { name: topic.name })
+        .getOne();
+    if (foundTopic === undefined) {
+        return topicsRepo.save(topic);
+    } else {
+        return foundTopic;
+    }
+}
+
+interface IGetTopic {
+    input: DeepPartial<Topic>;
+}
+
+// tslint:disable-next-line: no-any
+function getTopic(parent: any, args: IGetTopic, ctx: any, info: GraphQLResolveInfo): Promise<Topic | undefined> {
+    return _getTopic(args.input.id, args.input.name);
+}
+
+export async function _getTopic(id: number | undefined = undefined, name: string | undefined = undefined): Promise<Topic | undefined> {
+    if (typeof id === "number") {
+        return getConnection().createQueryBuilder()
+            .where("topic.id = :id", { id })
+            .getOne();
+    }
+
+    if (typeof name === "string") {
+        return getConnection().createQueryBuilder()
+            .where("topic.name = :name", { name })
+            .getOne();
+    }
+
+    return Promise.reject(undefined);
 }
 
 export const resolvers: IResolvers = {
+    Mutation: {
+        createTopic,
+    },
     Query: {
-        topic: (parent, args: ITopicQuery, context, info) => {
-            return getConnection()
-                .getRepository(Topic)
-                .createQueryBuilder("topic")
-                .where(`"topic"."id" = ${args.id}`)
-                .getOne()
-                .then(topic => {
-                    if (topic === undefined) { return undefined; }
-
-                    return topic;
-                });
-        },
+        getTopic,
     },
 };
--- bb-server/src/schema/User.ts
+++ bb-server/src/schema/User.ts
@@ -1,11 +1,15 @@
 import { gql, IResolvers } from "apollo-server-express";
 import { DocumentNode, GraphQLResolveInfo } from "graphql";
 import { DeepPartial, getConnection, Repository } from "typeorm";
+import Topic from "../entities/Topic";
 import User from "../entities/User";
+import { _createTopic, _getTopic } from "./Topic";
 
 export const typeDef: DocumentNode = gql`
     extend type Mutation {
         createUser(input: CreateUserInput!): User
+        updateUser(input: UpdateUserInput!): User
+        updateWhitelist(id: Int!, topics: [TopicInput]!): [Topic]
     }
 
     extend type Query {
@@ -41,8 +45,11 @@ interface ICreateUserInput {
 }
 
 // tslint:disable-next-line: no-any
-async function createUser(parent: any, args: ICreateUserInput, ctx: any, info: GraphQLResolveInfo): Promise<DeepPartial<User> | undefined> {
-    const userRepo: Repository<User> = getConnection().getRepository(User);
+function createUser(parent: any, args: ICreateUserInput, ctx: any, info: GraphQLResolveInfo): Promise<DeepPartial<User> | undefined> {
+    return _createUser(args.input);
+}
+
+export async function _createUser(newUser: DeepPartial<User>): Promise<DeepPartial<User> | undefined> {
     try {
         const user: DeepPartial<User> = await userRepo.save(args.input);
         return user;
@@ -52,13 +59,82 @@ async function createUser(parent: any, args: ICreateUserInput, ctx: any, info: G
     }
 }
 
-interface IUserQuery {
+interface IGetUser {
     id: number;
+}
+
+// tslint:disable-next-line: no-any
+function getUser(parent: any, args: IGetUser, ctx: any, info: GraphQLResolveInfo): Promise<User | undefined> {
+    if (args.id === undefined) {
+        return Promise.reject(undefined);
+    }
+
+    return args.id !== undefined ? _getUser(args.id) : Promise.reject(undefined);
+}
+
+export async function _getUser(id: number): Promise<User | undefined> {
+    return getConnection()
+        .getRepository(User)
+        .findOne({
+            where: {
+                id,
+            },
+            relations: [
+                "hostedMeals", "whitelist", "blacklist",
+                "reviews", "userReviewsAuthored", "recipeReviewsAuthored",
+                "recipesAuthored",
+            ],
+        });
+}
+
+interface IUpdateUser {
+    input: DeepPartial<User>;
+}
+
+// tslint:disable-next-line: no-any
+function updateUser(parent: any, args: IUpdateUser, ctx: any, info: GraphQLResolveInfo): Promise<User | undefined> {
+    return args.input.id !== undefined ? _updateUser(args.input) : Promise.reject(undefined);
+}
+
+export async function _updateUser(input: DeepPartial<User>): Promise<User | undefined> {
+    if (input.id === undefined) {
+        return undefined;
+    }
+    try {
+        const user: User | undefined = await getConnection().getRepository(User).save({
+            ..._getUser(input.id),
+            ...input,
+        });
+        return user;
+    } catch (reason) {
+        console.log(reason);
+        return undefined;
+    }
+}
+
+export async function _updateWhitelist(id: number, topics: DeepPartial<Topic>[]): Promise<Topic[] | undefined> {
+    const topicsRepo: Repository<Topic> = getConnection().getRepository(Topic);
+    try {
+        const user: User = await _getUser(id);
+        for (const topic of topics) {
+            if (await _getTopic(undefined, topic.name) === undefined) {
+                await _createTopic({ name: topic.name });
+            }
+        }
+
+        const names: (string | undefined)[] = topics.map(topic => topic.name);
+    } catch (reason) {
+        console.log(reason);
+        return undefined;
+    }
 }
 
 export const resolvers: IResolvers = {
     Mutation: {
         createUser,
+        updateUser,
+        updateWhitelist,
+        // updateBlacklist,
     },
     Query: {
         user: async (parent, args: IUserQuery, context, info) => {
