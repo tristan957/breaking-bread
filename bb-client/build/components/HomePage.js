8fe321f98c51c35b5db4e23f4e8fbfe7fc5c0af7
Jonathan Wang <jonathan.wang1996@gmail.com>
2018-10-17 18:59:00 -0500

    yeet

--- bb-client/build/components/HomePage.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-// import Background from '../../images/background.jpeg';
-class HomePage extends Component {
-    render() {
-        return (React.createElement("div", null, "Home page"));
-    }
-}
-export default HomePage;
-//# sourceMappingURL=HomePage.js.map
