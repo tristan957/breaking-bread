f2e1a4352b34efa6183b15a5fd095b547c910977
Jonathan Wang <jonathan.wang1996@gmail.com>
2018-10-30 21:50:03 -0500

    me hoy minoy

--- bb-client/src/auth/WebAuthentication.tsx
+++ bb-client/src/auth/WebAuthentication.tsx
@@ -1,13 +1,13 @@
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { setContext } from "apollo-link-context";
+import { HttpLink } from "apollo-link-http";
 import { Auth0DecodedHash, Auth0Error, WebAuth } from "auth0-js";
 import autobind from "autobind-decorator";
+import gql from "graphql-tag";
 import history from "../history";
 import { Auth0Authentication } from "./Auth0Authentication";
 import { AUTH_CONFIG } from "./configuration";
-import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
-import { setContext } from "apollo-link-context";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import gql from "graphql-tag";
 /**
  * Web based Auth0 authentication
  *
@@ -18,15 +18,15 @@ import gql from "graphql-tag";
 
 const httpLink = new HttpLink({
 	uri: "http://localhost:4000/graphql",
-  });
+});
 
 const authLink = setContext((_, { headers }) => {
 	const token = localStorage.getItem("access_token");
 	// return the headers to the context so httpLink can read them
 	return {
 		headers: {
-		...headers,
-		authorization: token ? `Bearer ${token}` : undefined,
+			...headers,
+			authorization: token ? `Bearer ${token}` : undefined,
 		},
 	};
 });
@@ -110,12 +110,12 @@ export class WebAuthentication implements Auth0Authentication {
 
 	@autobind
 	public setUser(): void {
-	 apolloClient
-		.query({
-		  query: gql`{message}`,
-		}).then(res => {
-		const data: any = res.data;
-		localStorage.setItem("user_id", data.message);
-	  });
+		apolloClient
+			.query({
+				query: gql`{message}`,
+			}).then(res => {
+				const data: any = res.data;
+				localStorage.setItem("user_id", data.message);
+			});
 	}
 }
