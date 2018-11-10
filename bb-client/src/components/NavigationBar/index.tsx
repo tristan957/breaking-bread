e3e42a786883a19bda1dd2c658a2b74318c406ca
Jonathan Wang <jonathan.wang1996@gmail.com>
2018-11-10 12:53:27 -0600

    Changed navigation bar to modal

--- bb-client/src/components/NavigationBar/index.tsx
+++ bb-client/src/components/NavigationBar/index.tsx
@@ -2,13 +2,12 @@ import moment from "moment";
 import React from "react";
 import { SingleDatePicker } from "react-dates";
 import "react-dates/initialize";
-import { Button, Collapse, Form, FormGroup, Input, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem } from "reactstrap";
+import { Button, Col, Form, FormGroup, Input, Label, Nav, Navbar, NavbarBrand, NavItem } from "reactstrap";
 import Rodal from "rodal";
 import "../resources/css/NavigationBar.css";
 import logo from "../resources/images/logo_icon.png";
 
 interface IAppState {
-	isOpen: boolean;
 	createdAt: moment.Moment | null;
 	calendarFocused: boolean;
 	visible: boolean;
@@ -17,32 +16,24 @@ interface IAppState {
 export default class NavigationBar extends React.Component<{}, IAppState> {
 	constructor(props: Readonly<{}>) {
 		super(props);
-		this.toggle = this.toggle.bind(this);
 		this.onDateChange = this.onDateChange.bind(this);
 		this.onFocusChange = this.onFocusChange.bind(this);
 		this.state = {
-			isOpen: false,
 			createdAt: moment(),
 			calendarFocused: false,
 			visible: false,
 		};
 	}
 
-	public show(): void {
+	public showModal(): void {
 		this.setState({
 			visible: true,
 		});
 	}
 
-	public hide(): void {
+	public hideModal(): void {
 		this.setState({
 			visible: false,
-		});
-	}
-
-	public toggle(): void {
-		this.setState({
-			isOpen: !this.state.isOpen,
 		});
 	}
 
@@ -65,52 +56,59 @@ export default class NavigationBar extends React.Component<{}, IAppState> {
 					<NavbarBrand href="/">
 						<span><img src={logo} height="40" /></span>
 					</NavbarBrand>
-					<NavbarToggler onClick={this.toggle} />
-					<Collapse isOpen={this.state.isOpen} navbar>
-						<Nav className="ml-auto" navbar>
-							<NavItem>
-								<Form>
-									<FormGroup>
-										<Input type="search" name="search" id="search" placeholder="Search" />
-									</FormGroup>
-								</Form>
-							</NavItem>
+					<Nav className="ml-auto" navbar>
+						<NavItem>
+							{/* This button triggers our modal (Rodal) */}
+							<Button className="float-right" type="submit" onClick={this.showModal.bind(this)} id="login">Filters</Button>
+						</NavItem>
+					</Nav>
+				</Navbar>
 
-							<NavItem>
-								<Form>
+				<Rodal visible={this.state.visible} onClose={this.hideModal.bind(this)}>
+					<div className="header">Filters</div>
+					<div className="body">
+						<Form>
+							<FormGroup row>
+								<Label for="search" sm={2}>Search</Label>
+								<Col sm={10}>
+									<Input name="search" id="search" />
+								</Col>
+							</FormGroup>
+
+							<FormGroup row>
+								<Label for="guest" sm={2}>Guest</Label>
+								<Col sm={10}>
 									<Input type="select" name="guest" id="guest">
-										<option value="" disabled selected>Guests</option>
+										<option value="Guests" disabled>Guests</option>
 										<option>1</option>
 										<option>2</option>
 										<option>3</option>
 										<option>4</option>
 										<option>5</option>
 									</Input>
-								</Form>
-							</NavItem>
+								</Col>
+							</FormGroup>
 
-							<NavItem>
-								<SingleDatePicker
-									date={this.state.createdAt}
-									focused={this.state.calendarFocused}
-									onDateChange={this.onDateChange}
-									onFocusChange={this.onFocusChange}
-									id={"datepicker"}
-									small={true}
-								/>
-							</NavItem>
+							<FormGroup row>
+								<Label for="date" sm={2}>Date</Label>
+								<Col sm={10}>
+									<SingleDatePicker
+										date={this.state.createdAt}
+										focused={this.state.calendarFocused}
+										onDateChange={this.onDateChange}
+										onFocusChange={this.onFocusChange}
+										id={"datepicker"}
+										small={true}
+										numberOfMonths={1}
+									/>
+								</Col>
+							</FormGroup>
 
-							<NavItem>
-								<Button type="submit" onClick={this.show.bind(this)} id="login">Modal Test</Button>
-							</NavItem>
-						</Nav>
-					</Collapse>
-				</Navbar>
-				<Rodal width={800} height={500} visible={this.state.visible} onClose={this.hide.bind(this)}>
-					<div className="header">modal test</div>
-					<div className="body">What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little "clever" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.</div>
+							<Button className="float-right"> Submit </Button>
+						</Form>
+					</div>
 				</Rodal>
-			</div >
+			</div>
 		);
 	}
 }
