15e870e5ee74e3fa2b8bacd761bd92cb54693d77
Jonathan Wang <jonathan.wang1996@gmail.com>
2018-11-28 19:40:33 -0600

    added templates

--- bb-mailer/src/Mailer.ts
+++ bb-mailer/src/Mailer.ts
@@ -1,19 +1,46 @@
 import * as aws from "aws-sdk";
 import * as nodemailer from "nodemailer";
+const Email = require('email-templates');
 
+// setup/config stuff
 aws.config.loadFromPath("./config.json");
 var transporter = nodemailer.createTransport({
 	SES: new aws.SES({
 		apiVersion: '2010-12-01'
 	})
 });
-
-transporter.sendMail({
-	from: 'no-reply@bbread.org',
-	to: 'jonathan.wang1996@gmail.com',
-	subject: 'Test Subject',
-	html: 'Hello World!',
-}, function (err, data) {
-	if (err) console.log(err, err.stack);
-	else console.log(data);
+var email = new Email({
+	send: true,				// Set false to test in dev
+	preview: false,			// Set true to preview emails
+	transport: transporter
 });
+
+// templates
+var greeting = {
+	template: 'greeting',	// template is based off folder name in emails
+	message: {
+		from: 'no-reply@bbread.org',
+		to: 'jonathan.wang1996@gmail.com'
+	},
+	locals: {
+		name: 'Jon'			// variables used for pug files
+	}
+}
+var testing = {
+	template: 'testing',	// template is based off folder name in emails
+	message: {
+		from: 'no-reply@bbread.org',
+		to: 'jonathan.wang1996@gmail.com'
+	},
+	locals: {
+		var1: 'Test variable 1',	// variables used for pug files
+		var2: 'Test variable 2'
+	}
+}
+
+// send email
+function sendEmail(opt: any) {
+	email.send(opt).then(console.log).catch(console.error);
+}
+sendEmail(greeting);
+sendEmail(testing);
