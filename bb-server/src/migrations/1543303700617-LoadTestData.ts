f5c3794e5fa42c48ea38b5a8bbe8ff23f2d37324
Greg Noonan <gregnoonan@tamu.edu>
2018-11-28 20:22:38 -0600

    tslint baby

--- bb-server/src/migrations/1543303700617-LoadTestData.ts
+++ bb-server/src/migrations/1543303700617-LoadTestData.ts
@@ -2,12 +2,12 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class LoadTestData1543303700617 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        // load load_test_data.sql into string and execute
-    }
+	public async up(queryRunner: QueryRunner): Promise<any> {
+		// load load_test_data.sql into string and execute
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-        // load delete_test_data.sql into string and execute
-    }
+	public async down(queryRunner: QueryRunner): Promise<any> {
+		// load delete_test_data.sql into string and execute
+	}
 
 }
