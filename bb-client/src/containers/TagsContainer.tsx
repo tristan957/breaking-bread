5e1cb485bdb7a9afdab9b761b564443ff2908975
Tristan Partin <tristan.partin@gmail.com>
2018-12-03 11:51:17 -0600

    Get the add tag by name working

--- bb-client/src/containers/TagsContainer.tsx
+++ bb-client/src/containers/TagsContainer.tsx
@@ -1,7 +1,9 @@
 import gql from "graphql-tag";
 import React from "react";
-import { Query, QueryResult } from "react-apollo";
+import { Mutation, MutationFn, MutationResult, Query, QueryResult } from "react-apollo";
+import { Button, Input, InputGroup, InputGroupAddon } from "reactstrap";
 import Tags from "../components/Tags";
+import Tag from "../entities/Tag";
 import User from "../entities/User";
 import "./resources/css/TagsContainer.css";
 
@@ -27,12 +29,42 @@ interface IUserFollowedTagsVariables {
 
 type UserFollowedTagsResult = QueryResult<IUserFollowedTagsData, IUserFollowedTagsVariables>;
 
+const USER_TOGGLE_FOLLOWED_TAGS = gql`
+	mutation UserToggleFollowedTags($tags: [TagInput!]!) {
+		userToggleFollowedTags(tags: $tags) {
+			id
+			name
+		}
+	}
+`;
+
+interface IUserToggleFollowedTagsData {
+	userToggleFollowedTags: Partial<Tag>[];
+}
+
+interface IUserToggleFollowedTagsVariables {
+	tags: Partial<Tag>[];
+}
+
+type UserToggleFollowedTagsResult = MutationResult<IUserToggleFollowedTagsData>;
+type UserToggleFollowedTagsFn = MutationFn<IUserToggleFollowedTagsData, IUserToggleFollowedTagsVariables>;
+
 interface ITagsContainerProps {
 	mutable?: boolean;
 	userID: number;
 }
 
-export default class TagsContainer extends React.Component<ITagsContainerProps> {
+interface ITagsContainerState {
+	name?: string;
+}
+
+export default class TagsContainer extends React.Component<ITagsContainerProps, ITagsContainerState> {
+	constructor(props: ITagsContainerProps) {
+		super(props);
+
+		this.state = {};
+	}
+
 	public render(): JSX.Element {
 		return (
 			<Query query={USER_FOLLOWED_TAGS} variables={{ id: this.props.userID }}>
@@ -45,7 +77,34 @@ export default class TagsContainer extends React.Component<ITagsContainerProps> 
 
 					return (
 						<div id="followed-tags-card" className="card">
-							<div id="tags-topics-list-header" className="container-header">Tags</div>
+							<div id="tags-list-header" className="container-header">
+								<div className="container-header" id="tags-label">Tags</div>
+								{this.props.mutable
+									? (
+										<Mutation mutation={USER_TOGGLE_FOLLOWED_TAGS} onCompleted={() => result.refetch()}>
+											{(userToggleFollowedTags: UserToggleFollowedTagsFn, mResult: UserToggleFollowedTagsResult) => {
+												if (result.error) {
+													console.error(result.error);
+													return <div>{result.error.message}</div>;
+												}
+
+												return (
+													<InputGroup id="tag-input">
+														<Input onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.setState({ name: e.target.value })} />
+														<InputGroupAddon addonType="append">
+															<Button color="success" id="topic-add" onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+																userToggleFollowedTags({ variables: { tags: [{ name: this.state.name }] } });
+															}}>
+																+
+															</Button>
+														</InputGroupAddon>
+													</InputGroup>
+												);
+											}}
+										</Mutation>
+									) : undefined
+								}
+							</div>
 							<hr className="separator" />
 							<Tags
 								tags={result.data!.user.followedTags || []}
