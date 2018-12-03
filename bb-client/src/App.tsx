1837a52dbd73f91d51d20e178bb680b2ac9ec487
Greg Noonan <gregnoonan@tamu.edu>
2018-12-03 05:01:11 -0600

    Ugly reload

--- bb-client/src/App.tsx
+++ bb-client/src/App.tsx
@@ -1,6 +1,12 @@
+import { InMemoryCache } from "apollo-cache-inmemory";
+import ApolloClient from "apollo-client";
+import { ApolloLink } from "apollo-link";
+import { BatchHttpLink } from "apollo-link-batch-http";
+import { ErrorResponse, onError } from "apollo-link-error";
+import { ServerError } from "apollo-link-http-common";
 import gql from "graphql-tag";
 import React from "react";
-import { Query, QueryResult } from "react-apollo";
+import { ApolloProvider, Query, QueryResult } from "react-apollo";
 import { Route, Switch } from "react-router";
 import NotFound from "../src/components/NotFound";
 import { WebAuthentication } from "./auth/WebAuthentication";
@@ -32,47 +38,85 @@ interface IUserAuthenticatedResult {
 	} | null;
 }
 
+const uri = process.env.NODE_ENV === "development" ? "http://localhost:10262/graphql" : "https://www.bbread.org/api/v1/graphql";
+console.log(`URI: ${uri}`);
+
 // tslint:disable-next-line: variable-name
 export const UserContext = React.createContext<IAppContext>({ userID: undefined, reloadUser: undefined, auth: undefined });
 
 export default class App extends React.Component {
 	public render(): JSX.Element {
+		let accessToken: string | null = localStorage.getItem("access_token");
+		accessToken = accessToken === null ? "" : accessToken;
+		const client = new ApolloClient({
+			link: ApolloLink.from([
+				onError((error: ErrorResponse) => {
+					console.log("I AM AN ERROR");
+					if (error.graphQLErrors) {
+						error.graphQLErrors.map(graphQLError => {
+							console.log(
+								`[GraphQL error]: Message: ${graphQLError.message}, Location: ${graphQLError.locations}, Path: ${graphQLError.path}`
+							);
+						});
+					}
+
+					if (error.networkError) {
+						console.log(`[Network error]: ${error.networkError}`);
+						const e = error.networkError as ServerError;
+						if (e.statusCode === 404) {
+							console.log("I am a 404 error");
+							document.write("<h1>you suck</h1>");
+						}
+					}
+				}),
+				new BatchHttpLink({
+					uri,
+					headers: {
+						token_bearer: accessToken,
+					},
+				}),
+			]),
+			cache: new InMemoryCache(),
+		});
+
 		return (
-			<Query query={USER_AUTHENTICATED}>
-				{(result: QueryResult<IUserAuthenticatedResult>) => {
-					if (result.loading) {
-						return <div></div>;
-					}
-					if (result.error) {
-						console.log(result.error);
-						return <div>{result.error.message}</div>;
-					}
+			<ApolloProvider client={client}>
+				<Query query={USER_AUTHENTICATED}>
+					{(result: QueryResult<IUserAuthenticatedResult>) => {
+						if (result.loading) {
+							return <div></div>;
+						}
+						if (result.error) {
+							console.log(result.error);
+							return <div>{result.error.message}</div>;
+						}
 
-					const auth: WebAuthentication = new WebAuthentication();
+						const auth: WebAuthentication = new WebAuthentication();
 
-					return (
-						<div>
-							<div id="top">
-								<NavigationBar auth={auth} />
-							</div>
-							<div id="page-content">
-								<div id="content-container">
-									<UserContext.Provider value={{ userID: result.data!.userAuthenticated === null ? undefined : result.data!.userAuthenticated!.id, reloadUser: () => result.refetch(), auth }}>
-										<Switch>
-											<Route exact path="/" component={DashboardPage} />
-											<Route exact path="/m/:mealID" component={MealPage} />
-											<Route exact path="/p/:userID" component={ProfilePage} />
-											{/* <Route exact path="/r/:recipeID" component={RecipePage} /> */}
-											<Route exact path="/bb-auth" component={AuthCallbackPage} />
-											<Route component={NotFound} />
-										</Switch>
-									</UserContext.Provider>
+						return (
+							<div>
+								<div id="top">
+									<NavigationBar auth={auth} />
+								</div>
+								<div id="page-content">
+									<div id="content-container">
+										<UserContext.Provider value={{ userID: result.data!.userAuthenticated === null ? undefined : result.data!.userAuthenticated!.id, reloadUser: () => result.refetch(), auth }}>
+											<Switch>
+												<Route exact path="/" component={DashboardPage} />
+												<Route exact path="/m/:mealID" component={MealPage} />
+												<Route exact path="/p/:userID" component={ProfilePage} />
+												{/* <Route exact path="/r/:recipeID" component={RecipePage} /> */}
+												<Route exact path="/bb-auth" component={AuthCallbackPage} />
+												<Route component={NotFound} />
+											</Switch>
+										</UserContext.Provider>
+									</div>
 								</div>
 							</div>
-						</div>
-					);
-				}}
-			</Query>
+						);
+					}}
+				</Query>
+			</ApolloProvider>
 		);
 	}
 }
--- bb-client/src/index.tsx
+++ bb-client/src/index.tsx
@@ -1,13 +1,7 @@
-import { InMemoryCache } from "apollo-cache-inmemory";
-import ApolloClient from "apollo-client";
-import { ApolloLink } from "apollo-link";
-import { BatchHttpLink } from "apollo-link-batch-http";
-import { ErrorResponse, onError } from "apollo-link-error";
-import { ServerError } from "apollo-link-http-common";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import dotenv from "dotenv";
 import React from "react";
-import { ApolloProvider } from "react-apollo";
 import "react-dates/lib/css/_datepicker.css";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -17,48 +11,9 @@ import * as serviceWorker from "./serviceWorker";
 
 dotenv.config();
 
-const uri = process.env.NODE_ENV === "development" ? "http://localhost:10262/graphql" : "https://www.bbread.org/api/v1/graphql";
-console.log(`URI: ${uri}`);
-
-let accessToken: string | null = localStorage.getItem("access_token");
-accessToken = accessToken === null ? "" : accessToken;
-
-const client = new ApolloClient({
-	link: ApolloLink.from([
-		onError((error: ErrorResponse) => {
-			console.log("I AM AN ERROR");
-			if (error.graphQLErrors) {
-				error.graphQLErrors.map(graphQLError => {
-					console.log(
-						`[GraphQL error]: Message: ${graphQLError.message}, Location: ${graphQLError.locations}, Path: ${graphQLError.path}`
-					);
-				});
-			}
-
-			if (error.networkError) {
-				console.log(`[Network error]: ${error.networkError}`);
-				const e = error.networkError as ServerError;
-				if (e.statusCode === 404) {
-					console.log("I am a 404 error");
-					document.write("<h1>you suck</h1>");
-				}
-			}
-		}),
-		new BatchHttpLink({
-			uri,
-			headers: {
-				token_bearer: accessToken,
-			},
-		}),
-	]),
-	cache: new InMemoryCache(),
-});
-
 ReactDOM.render(
 	<BrowserRouter>
-		<ApolloProvider client={client}>
-			<App />
-		</ApolloProvider>
+		<App />
 	</BrowserRouter>,
 	document.getElementById("root")
 );
