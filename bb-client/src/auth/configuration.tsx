1bdae7efd0893704bd8580f39ec6367e567a9dc8
Greg Noonan <gregnoonan@tamu.edu>
2018-12-12 06:20:35 -0600

    auth for partin.io

--- bb-client/src/auth/configuration.tsx
+++ bb-client/src/auth/configuration.tsx
@@ -1,9 +1,9 @@
 import { Auth0Config } from "./Auth0Config";
 export const AUTH_CONFIG: Auth0Config = {
-  audience: "https://www.bbread.org/api/v1/graphql",
+  audience: "https://www.partin.io/bread/api/v1/graphql",
   domain: "bbread.auth0.com",
   clientId: "5Km8K7jPRTeT260MGPtJSOzPb5tgqKYY",
-  callbackUrl: "http://localhost:3000/bb-auth",
+  callbackUrl: "https://partin.io/bb-auth",
 
   // Jonathan's credential
   // domain: "breaking-bread.auth0.com",
